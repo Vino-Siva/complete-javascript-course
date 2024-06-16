fb8a6e12dd4ddcdb1b58b03992edba3c2b2ab324
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-06-16 20:52:39 +0200

    Completed coding challenge 3 of OOP Section

--- 14-OOP/codingChallenges/script.js
+++ 14-OOP/codingChallenges/script.js
@@ -48,3 +48,29 @@ ford.accelerate();
 ford.brake();
 
 console.log(ford);
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV;
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+const tesla = new EV('Tesla', 120, 23);
+tesla.accelerate();
+tesla.brake();
+tesla.chargeBattery(90);
+tesla.accelerate();
+console.log(tesla instanceof EV);
+console.log(tesla instanceof Car);
+console.log(tesla instanceof Object);
+console.dir(EV.prototype.constructor);
